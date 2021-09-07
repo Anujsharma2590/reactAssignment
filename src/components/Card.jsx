7efd28781924d7e2894eab6bca3aa6d894e6a723
Anuj sharma <aanuj.sharma2590@gmail.com>
2021-09-07 08:15:39 +0530

    sec commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,43 @@
+import './Card.css'
+
+
+export function Card() {
+    return (
+      <div>
+        <div>
+          <h1>Mobile Operating System</h1>
+          <ul>
+            <li>
+              <span>BalckBerry</span>
+            </li>
+            <li>
+              <span>iPhone</span>
+            </li>
+            <li>
+              <span>Window Phone</span>
+            </li>
+            <li>
+              <span>Android</span>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1>Mobile Manufactures</h1>
+          <ul>
+            <li>
+              <span> Samsung</span>
+            </li>
+            <li>
+              <span>HTC</span>
+            </li>
+            <li>
+              <span>Micromax</span>
+            </li>
+            <li>
+              <span>Apple</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+}
